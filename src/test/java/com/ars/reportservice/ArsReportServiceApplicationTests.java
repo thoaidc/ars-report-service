5de2165902c3a450ae3e55da9ab7cc8f6d7e94e5
thoaidc <dcthoai1023@gmail.com>
2025-08-29 23:07:55 +0700

    init

---
+++ src/test/java/com/ars/reportservice/ArsReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ars.reportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsReportServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
